9a24d094e3b09112bdb185f534d226f7bc0e95b6
Leigh <1142961+leighgordy@users.noreply.github.com>
2026-09-03 16:32:51 +0100

    Removed big apps being silly, you don't need an AI to produce it, just  git

--- apps/react-spa/src/components/App.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const HeaderOne = styled.h1`
-  color: green;
-`;
-
-export default function App() {
-  return <HeaderOne> Hello, React + TypeScript + Vite!</HeaderOne>;
-}
