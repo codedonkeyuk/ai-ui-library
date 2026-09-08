3d31c39071ca8973fba33ce1894150d9207b00d0
Leigh <1142961+leighgordy@users.noreply.github.com>
2026-09-08 09:15:54 +0100

    Fixed style issues with loader

--- src/lib/components/Loading.tsx
+++ src/lib/components/Loading.tsx
@@ -1,9 +1,13 @@
 import type { JSX } from "react/jsx-runtime";
 
-/** Renders a accessible loading div. Necessary for dynamic import react suspend logic*/
+/**
+ * Accessible loading spinner for use when components are loading. Pulls css from Loading.css.
+ *
+ * Loading.css is a separate CSS file as it needed pfor when the app is loading.
+ * */
 export default function Loading(): JSX.Element {
   return (
-    <div className="spinner-container">
+    <div className="message-container">
       <div className="loading-spinner" role="status" aria-label="Loading"></div>
     </div>
   );
